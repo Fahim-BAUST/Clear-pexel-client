782fee96dbea866cd208a0ce5e7aab107de6f904
Fahim-BAUST <ashhabf50@gmail.com>
2021-11-21 22:18:26 +0600

    banner changes

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,13 +3,13 @@ import Font from 'react-font'
 
 const Banner = () => {
     return (
-        <div >
+        <div className="mt-5">
             <Font family="Indie Flower">
-                <h1 className="mt-4 pt-4 mb-5 fw-bold text-center site-name " style={{
+                <h1 className="mt-5 pt-4 mb-5 fw-bold text-center site-name " style={{
                     color: "white", fontSize: "75px", position: "absolute",
                     left: "50%", transform: "translate(-50%)"
                 }}>CLEAR PIXEL</h1></Font>
-            <img className="img-fluid w-100 " src="https://i.ibb.co/tKgGMk5/top-view-photography-accesories-with-copy-space-1-1.jpg" alt="" />
+            <img className="img-fluid w-100 " src="https://i.ibb.co/0DTX07F/top-view-ph.jpg" alt="" />
 
         </div>
     );
